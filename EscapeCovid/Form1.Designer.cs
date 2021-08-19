2f5b55a546a8b0d2b11f7df92587f4fd54a55868
martinpoposki27 <poposkimar@gmail.com>
2021-08-19 16:03:10 +0200

    player can walk

---
+++ EscapeCovid/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace EscapeCovid
+{
+    partial class EscapeCovid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtKills = new System.Windows.Forms.Label();
+            this.txtImunitet = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 9);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(131, 25);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Вакцини: 0";
+            // 
+            // txtKills
+            // 
+            this.txtKills.AutoSize = true;
+            this.txtKills.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKills.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtKills.Location = new System.Drawing.Point(270, 9);
+            this.txtKills.Name = "txtKills";
+            this.txtKills.Size = new System.Drawing.Size(183, 25);
+            this.txtKills.TabIndex = 1;
+            this.txtKills.Text = "Вакцинирани: 0";
+            // 
+            // txtImunitet
+            // 
+            this.txtImunitet.AutoSize = true;
+            this.txtImunitet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImunitet.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtImunitet.Location = new System.Drawing.Point(571, 9);
+            this.txtImunitet.Name = "txtImunitet";
+            this.txtImunitet.Size = new System.Drawing.Size(124, 25);
+            this.txtImunitet.TabIndex = 2;
+            this.txtImunitet.Text = "Имунитет:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(701, 11);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(211, 23);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::EscapeCovid.Properties.Resources.up1;
+            this.player.Location = new System.Drawing.Point(342, 231);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(70, 60);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // EscapeCovid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtImunitet);
+            this.Controls.Add(this.txtKills);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "EscapeCovid";
+            this.Text = "Escape Covid Game <3";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtKills;
+        private System.Windows.Forms.Label txtImunitet;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
